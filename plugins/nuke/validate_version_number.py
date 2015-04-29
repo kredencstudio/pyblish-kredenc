409f47d12958a1debeee52b1f69343f3c039171d
Milan Kolar <mail@mkolar.com>
2015-04-29 16:28:17 +0200

    Start of making version validation independent from Nuke. Adding function for getting, setting and increasing version in a string (path). Still relies on Nuke to get the actual paths. These should be put to a function that will switch between different hosts.

--- plugins/nuke/validate_version_number.py
+++ plugins/nuke/validate_version_number.py
@@ -1,26 +1,33 @@
-import operator
-import traceback
+import pyblish.api
+import re
 
 import nuke
-import nukescripts
-import pyblish.api
 
 
 @pyblish.api.log
 class ValidateVersionNumber(pyblish.api.Validator):
-    """Validates the version number of write nodes compared to the file name
+    """Validates whether workFile is versioned and makes sure that version number of write nodes matches version of the
+    work file
     """
 
-    families = ['writeNode', 'prerenders']
+    families = ['writeNode', 'prerenders', 'workFile']
     hosts = ['nuke']
     version = (0, 1, 0)
 
     def process_instance(self, instance):
         path = nuke.root().name()
-        version_number = int(nukescripts.version_get(path, 'v')[1])
 
-        path = instance[0]['file'].value()
-        v = int(nukescripts.version_get(path, 'v')[1])
+        version_number = int(self.version_get(path, 'v')[1])
+
+        try:
+            path = instance[0]['file'].value()
+            v = int(self.version_get(path, 'v')[1])
+        except:
+            v = version_number
+
+        instance.context.set_data('version', value=version_number)
+        new_workfile = self.version_up(path)
+        instance.set_data('new_workfile', value=new_workfile)
 
         if version_number != v:
             msg = 'Version number %s is not the same as ' % v
@@ -31,10 +38,57 @@ class ValidateVersionNumber(pyblish.api.Validator):
         """Sets the version number of the output to the same as the file name
         """
         path = nuke.root().name()
-        version_number = int(nukescripts.version_get(path, 'v')[1])
+        version_number = int(self.version_get(path, 'v')[1])
 
         path = instance[0]['file'].value()
-        v = int(nukescripts.version_get(path, 'v')[1])
+        v = int(self.version_get(path, 'v')[1])
 
-        new_path = nukescripts.version_set(path, 'v', v, version_number)
+        new_path = self.version_set(path, 'v', v, version_number)
         instance[0]['file'].setValue(new_path)
+
+
+    def version_get(self, string, prefix, suffix = None):
+        """Extract version information from filenames.  Code from Foundry's nukescripts.version_get()"""
+
+        if string is None:
+           raise ValueError, "Empty version string - no match"
+
+        regex = "[/_.]"+prefix+"\d+"
+        matches = re.findall(regex, string, re.IGNORECASE)
+        if not len(matches):
+            msg = "No \"_"+prefix+"#\" found in \""+string+"\""
+            raise ValueError, msg
+        return (matches[-1:][0][1], re.search("\d+", matches[-1:][0]).group())
+
+
+    def version_set(self, string, prefix, oldintval, newintval):
+        """Changes version information from filenames. Code from Foundry's nukescripts.version_set()"""
+
+        regex = "[/_.]"+prefix+"\d+"
+        matches = re.findall(regex, string, re.IGNORECASE)
+        if not len(matches):
+            return ""
+
+        # Filter to retain only version strings with matching numbers
+        matches = filter(lambda s: int(s[2:]) == oldintval, matches)
+
+        # Replace all version strings with matching numbers
+        for match in matches:
+            # use expression instead of expr so 0 prefix does not make octal
+            fmt = "%%(#)0%dd" % (len(match) - 2)
+            newfullvalue = match[0] + prefix + str(fmt % {"#": newintval})
+            string = re.sub(match, newfullvalue, string)
+        return string
+
+    def version_up(self, string):
+
+        try:
+            (prefix, v) = self.version_get(string, 'v')
+            v = int(v)
+            file = self.version_set(string, prefix, v, v+1)
+        except:
+            raise ValueError, 'Unable to version up File'
+
+        return file
+
+
