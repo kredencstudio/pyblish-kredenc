a356fbcd6737e77d53e03ad3494670010e21dddb
Milan Kolar <mail@mkolar.com>
2015-05-06 17:17:37 +0200

    changin rop selector to flipbook and mantra selectors

---
+++ plugins/houdini/select_flipbook_nodes.py
@@ -0,0 +1,24 @@
+import hou
+import pyblish.api
+
+
+@pyblish.api.log
+class SelectFlipbookNodes(pyblish.api.Selector):
+    """Selects all write nodes"""
+
+    hosts = ['*']
+    version = (0, 1, 0)
+
+    def process_context(self, context):
+
+        renderNode = hou.node( "/out" )
+        render_nodes = renderNode.children()
+
+        for node in list(render_nodes):
+
+            if 'flipbook' in node.type().name():
+                instance = context.create_instance(name=node.name())
+                instance.set_data('family', value='preview')
+                instance.set_data('output_path', value=node.parm('outputV').eval())
+
+                instance.add(node)
