ac28d2bcc2d58e7a7aa3152cd1848facaca83c0b
Milan Kolar <mail@mkolar.com>
2015-05-12 14:19:44 +0200

    moving extract workfile to the bin. This is handled by conformer from now on.

--- plugins/bin/extract_workfile.py
+++ plugins/bin/extract_workfile.py
@@ -7,14 +7,13 @@ class PublishWorkfile(pyblish.api.Extractor):
     """Publishes current workfile to a _Publish location, next to current working directory"""
 
     families = ['workFile']
-    hosts = ['*']
+    hosts = ['']
     version = (0, 1, 0)
     optional = True
 
     def process_instance(self, instance):
         # submitting job
         sourcePath = instance.data('path')
-        new_workfile = instance.context.data('new_workfile')
         publishPath = instance.context.data('workfile_publish_path')
         shutil.copy(sourcePath, publishPath)
         instance.context.set_data('workfile_published', value=True)
