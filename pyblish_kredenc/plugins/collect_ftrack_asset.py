0b80735909482d748c02a9975e73331a23c52b43
Milan Kolar <mail@mkolar.com>
2016-04-04 23:23:04 +0200

    fix animation and looldev asset types

--- pyblish_kredenc/plugins/collect_ftrack_asset.py
+++ pyblish_kredenc/plugins/collect_ftrack_asset.py
@@ -13,16 +13,17 @@ class CollectFtrackAsset(pyblish.api.Collector):
 
         for instance in context:
 
+            self.log.info('instance {}'.format(instance))
             # skipping instance if ftrackData isn't present
             if not context.has_data('ftrackData'):
                 self.log.info('No ftrackData present. Skipping this instance')
-                return
+                continue
 
             # skipping instance if ftrackComponents isn't present
             if not instance.has_data('ftrackComponents'):
                 self.log.info('No ftrackComponents present\
                                Skipping this instance')
-                return
+                continue
 
             ftrack_data = context.data['ftrackData'].copy()
 
@@ -33,22 +34,28 @@ class CollectFtrackAsset(pyblish.api.Collector):
             task_type = ftrack_data['Task']['type'].lower()
             asset_type = ''
 
+            self.log.info('task type {}'.format(task_type))
+
             if task_type == 'lighting':
                 asset_type = 'render'
             if task_type == 'compositing':
                 asset_type = 'img'
             if task_type == 'lookdev':
-                asset_type = 'img'
+                asset_type = 'look'
             if task_type == 'modeling':
                 asset_type = 'geo'
             if task_type == 'rigging':
                 asset_type = 'rig'
+            if task_type == 'animation':
+                asset_type = 'anim'
 
             # family filtering
             if 'camera' in instance.data['family']:
                 asset_type = 'cam'
             if 'cache' in instance.data['family']:
                 asset_type = 'cache'
+            if 'render' in instance.data['family']:
+                asset_type = 'render'
 
             if asset_type:
                 instance.data['ftrackAssetType'] = asset_type
